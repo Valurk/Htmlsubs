246b18bbfdf75004432adcd3819e73cd610031c5
Valurk <73291263+Valurk@users.noreply.github.com>
2022-01-21 04:27:50 -0300

    Update htmlsubs.sh

--- htmlsubs.sh
+++ htmlsubs.sh
@@ -12,25 +12,28 @@ then echo -e "
 	echo "usage: $0 https://google.com"
 	
 else
-
-	 echo ">>>>>searching for subdomains!<<<<<"
-	 sleep 3
-	 
+	 	 	 
 	 wget $1 > /dev/null 2>&1
+	 
+	 		 
+	 grep href index.html | cut -d  "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<l" > result
+	 
+	 echo ">>>>> searching for subdomains! <<<<<"
+	 	 	 	 
+	 for url in $(cat result); do host $url | grep "has address" ; done >result2
 	 
 	 clear
 	 
-	 grep href index.html | cut -d  "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<l" > result
+	 sed -i 's/has address/>>>>>>>>>>/g' result2
 	 
-	 	  	 
-	 	 	 	 
-	 for url in $(cat result); do host $url | grep "has address"; done
-	 
-	 rm index.html
-	 
-		 
-	 #sort result | uniq #formatting the file
-	 #cat result	 
-	 
-fi
+	 echo "                 ▼▼▼▼▼▼ RESULTS ▼▼▼▼▼▼"
+	 echo '' 
+	 cat result2
 
+	 	 
+	 rm index.html
+	 	 
+	 #sort result | uniq #formatting the file
+	 #cat result
+	 	 
+fi
