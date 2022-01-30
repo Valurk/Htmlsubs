ddb02cdec474cad4095650e22a37d4f14b93cdcc
Valurk <73291263+Valurk@users.noreply.github.com>
2022-01-29 22:15:44 -0300

    Update htmlsubs.sh

--- htmlsubs.sh
+++ htmlsubs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-	if [ "$1" == "" ]
+if [ "$1" == "" ]
 then echo -e " \e[32m
 ██╗  ██╗████████╗███╗   ███╗██╗     ███████╗██╗   ██╗██████╗ ███████╗
 ██║  ██║╚══██╔══╝████╗ ████║██║     ██╔════╝██║   ██║██╔══██╗██╔════╝
@@ -9,32 +8,31 @@ then echo -e " \e[32m
 ██╔══██║   ██║   ██║╚██╔╝██║██║     ╚════██║██║   ██║██╔══██╗╚════██║
 ██║  ██║   ██║   ██║ ╚═╝ ██║███████╗███████║╚██████╔╝██████╔╝███████║
 ╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝
-"
-	echo "usage: $0 https://google.com"
-	
+    "
+    echo "usage: $0 https://google.com"
+    
 else
-	 	 	 
-	 wget $1 > /dev/null 2>&1
-	 
-	 		 
-	 grep href index.html | cut -d  "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<l" > result
-	 
-	 echo -e "\e[31;1m >>>>> searching for subdomains! <<<<<"
-	 	 	 	 
-	 for url in $(cat result); do host $url | grep "has address" ; done >results
-	 
-	 clear
-	 
-	 sed -i 's/has address/>>>>>>>>>>/g' results
-	 
-	 echo -e "\e[31;1m                ▼▼▼▼▼▼ RESULTS ▼▼▼▼▼▼"
-	 echo '' 
-	 
-	 sort results | uniq #formatting the file
-	 
-	 	 
-	 rm index.html
-	 rm result
-	 		 	 
+    
+    wget $1 > /dev/null 2>&1
+    
+    
+    grep href index.html | cut -d  "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<l" > result
+    
+    echo -e "\e[31;1m >>>>> searching for subdomains! <<<<<"
+    
+    for url in $(cat result); do host $url | grep "has address" ; done >results
+    
+    clear
+    
+    sed -i 's/has address/>>>>>>>>>>/g' results
+    
+    echo -e "\e[31;1m                ▼▼▼▼▼▼ RESULTS ▼▼▼▼▼▼"
+    echo ''
+    
+    sort results | uniq #formatting the file
+    
+    
+    rm index.html
+    rm result
+    
 fi
-
